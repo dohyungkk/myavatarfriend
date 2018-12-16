85882197cf269297d5aca5d54e38ba5539dd90ba
Denny Han <Dyhan68@gmail.com>
2018-12-16 13:31:05 -0800

    make it better

--- config/database.js
+++ config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
  'connection':{
   'host':'us-cdbr-iron-east-01.cleardb.net',
-  'user':'b455e0446cea14',
-  'password':'b07a0dd0'
+  'user':'b0593f3d678305',
+  'password':'2c3d1c89'
  },
- 'database':'heroku_09e897b20363e23',
+ 'database':'heroku_a54dea35f19a654',
  'user_table':'users'
 }
